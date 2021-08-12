2ccd00a3094d7b11657340a4cea02dde389712f6
aksr-aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-08-12 10:53:08 +0530

    Update backups.py

--- tg_bot/modules/backups.py
+++ tg_bot/modules/backups.py
@@ -320,22 +320,22 @@ def export_data(update, context):
         },
     }
     baccinfo = json.dumps(backup, indent=4)
-    f = open("YuiiChan{}.backup".format(chat_id), "w")
+    f = open("Vexana{}.backup".format(chat_id), "w")
     f.write(str(baccinfo))
     f.close()
     context.bot.sendChatAction(current_chat_id, "upload_document")
     tgl = time.strftime("%H:%M:%S - %d/%m/%Y", time.localtime(time.time()))
     context.bot.sendDocument(
         current_chat_id,
-        document=open("vexana{}.backup".format(chat_id), "rb"),
-        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `vexana-Backup` was specially made for notes.".format(
+        document=open("Vexana{}.backup".format(chat_id), "rb"),
+        caption="*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `Vexana-Backup` was specially made for notes.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("YuiiChan{}.backup".format(chat_id))  # Cleaning file
+    os.remove("Vexana{}.backup".format(chat_id))  # Cleaning file
 
 
 from tg_bot.modules.language import gs

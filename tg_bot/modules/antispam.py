0bab61fcba756968643d9082b402137e85442b72
aksr-aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-08-12 10:24:01 +0530

    Update antispam.py

--- tg_bot/modules/antispam.py
+++ tg_bot/modules/antispam.py
@@ -265,7 +265,7 @@ def gban(update: Update, context: CallbackContext):
             "#GBAN"
             "You have been marked as Malicious and as such have been banned from any future groups we manage."
             f"\n<b>Reason:</b> <code>{html.escape(user.reason)}</code>"
-            f"</b>Appeal Chat:</b> @yuiichansupport",
+            f"</b>Appeal Chat:</b> @vexanasupport",
             parse_mode=ParseMode.HTML,
         )
     except:
@@ -434,7 +434,7 @@ def check_and_ban(update, user_id, should_message=True):
             text = (
                 f"<b>Alert</b>: this user is globally banned.\n"
                 f"<code>*bans them from here*</code>.\n"
-                f"<b>Appeal chat</b>: @yuiichansupport\n"
+                f"<b>Appeal chat</b>: @vexanasupport\n"
                 f"<b>User ID</b>: <code>{user_id}</code>"
             )
             user = sql.get_gbanned_user(user_id)
@@ -519,7 +519,7 @@ def __user_info__(user_id):
         user = sql.get_gbanned_user(user_id)
         if user.reason:
             text += f"\n<b>Reason:</b> <code>{html.escape(user.reason)}</code>"
-        text += f"\n<b>Appeal Chat:</b> @yuiichansupport"
+        text += f"\n<b>Appeal Chat:</b> @vexanasupport"
     else:
         text = text.format("No")
     return text

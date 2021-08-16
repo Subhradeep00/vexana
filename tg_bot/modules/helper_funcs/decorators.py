ca900ec1de2f4b737624f656c0f1f284528fc6d2
kalya <stella.probot@gmail.com>
2021-08-16 21:26:38 +0530

    Update by Axel

--- tg_bot/modules/helper_funcs/decorators.py
+++ tg_bot/modules/helper_funcs/decorators.py
@@ -6,7 +6,7 @@ from typing import Optional, Union, List
 
 
 
-class VexanaTelegramHandler:
+class vexanaTelegramHandler:
     def __init__(self, d):
         self._dispatcher = d
 
@@ -25,7 +25,7 @@ class VexanaTelegramHandler:
                     self._dispatcher.add_handler(
                         CommandHandler(command, func, filters=filters, run_async=run_async, pass_args=pass_args), group
                     )
-                log.debug(f"[VexanaCMD] Loaded handler {command} for function {func.__name__} in group {group}")
+                log.debug(f"[vexanaCMD] Loaded handler {command} for function {func.__name__} in group {group}")
             except TypeError:
                 if can_disable:
                     self._dispatcher.add_handler(
@@ -35,7 +35,7 @@ class VexanaTelegramHandler:
                     self._dispatcher.add_handler(
                         CommandHandler(command, func, filters=filters, run_async=run_async, pass_args=pass_args, pass_chat_data=pass_chat_data)
                     )
-                log.debug(f"[VexanaCMD] Loaded handler {command} for function {func.__name__}")
+                log.debug(f"[vexanaCMD] Loaded handler {command} for function {func.__name__}")
 
             return func
 
@@ -52,7 +52,7 @@ class VexanaTelegramHandler:
                     self._dispatcher.add_handler(
                         MessageHandler(pattern, func, run_async=run_async), group
                     )
-                log.debug(f"[VexanaMSG] Loaded filter pattern {pattern} for function {func.__name__} in group {group}")
+                log.debug(f"[vexanaMSG] Loaded filter pattern {pattern} for function {func.__name__} in group {group}")
             except TypeError:
                 if can_disable:
                     self._dispatcher.add_handler(
@@ -62,7 +62,7 @@ class VexanaTelegramHandler:
                     self._dispatcher.add_handler(
                         MessageHandler(pattern, func, run_async=run_async)
                     )
-                log.debug(f"[VexanaMSG] Loaded filter pattern {pattern} for function {func.__name__}")
+                log.debug(f"[vexanaMSG] Loaded filter pattern {pattern} for function {func.__name__}")
 
             return func
         return _message
@@ -70,18 +70,18 @@ class VexanaTelegramHandler:
     def callbackquery(self, pattern: str = None, run_async: bool = True):
         def _callbackquery(func):
             self._dispatcher.add_handler(CallbackQueryHandler(pattern=pattern, callback=func, run_async=run_async))
-            log.debug(f'[VexanaCALLBACK] Loaded callbackquery handler with pattern {pattern} for function {func.__name__}')
+            log.debug(f'[vexanaCALLBACK] Loaded callbackquery handler with pattern {pattern} for function {func.__name__}')
             return func
         return _callbackquery
 
     def inlinequery(self, pattern: Optional[str] = None, run_async: bool = True, pass_user_data: bool = True, pass_chat_data: bool = True, chat_types: List[str] = None):
         def _inlinequery(func):
             self._dispatcher.add_handler(InlineQueryHandler(pattern=pattern, callback=func, run_async=run_async, pass_user_data=pass_user_data, pass_chat_data=pass_chat_data, chat_types=chat_types))
-            log.debug(f'[VexanaINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | PASSES USER DATA: {pass_user_data} | PASSES CHAT DATA: {pass_chat_data} | CHAT TYPES: {chat_types}')
+            log.debug(f'[vexanaINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | PASSES USER DATA: {pass_user_data} | PASSES CHAT DATA: {pass_chat_data} | CHAT TYPES: {chat_types}')
             return func
         return _inlinequery
 
-Vexanacmd = VexanaTelegramHandler(d).command
-Vexanamsg = VexanaTelegramHandler(d).message
-Vexanacallback = VexanaTelegramHandler(d).callbackquery
-Vexanainline = VexanaTelegramHandler(d).inlinequery
+vexanacmd = vexanaTelegramHandler(d).command
+vexanamsg = vexanaTelegramHandler(d).message
+vexanacallback = vexanaTelegramHandler(d).callbackquery
+vexanainline = vexanaTelegramHandler(d).inlinequery
